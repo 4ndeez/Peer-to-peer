7a78a37403d6ef30851bec678b60957c953c5a07
4ndeez <andissteam@gmail.com>
2019-06-20 18:28:13 +0300

    seeds minor fix
    add 3 accounts seeds, add safe navigation on user profiles

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,21 @@ indexes = [224000, 210000, 246000, 230000, 223010, 212000, 220000, nil]
 cities.each_with_index do |city, index|
   City.create(name: city, index: indexes[index])
 end
+
+emails = ['user@p2p.example', 'moderator@p2p.example', 'admin@p2p.example']
+
+User.roles.keys.each_with_index do |role, index, user = User.new|
+  user.email = emails[index]
+  user.nickname = Faker::TvShows::RickAndMorty.character
+  user.first_name = Faker::Name.first_name
+  user.second_name = Faker::Name.last_name
+  user.role = role
+  user.encrypted_password = '$2a$11$w3tai7ejKU89Em1cH8NFLeZ2NgZd39jzM4JLFWCIqazatYYlv6vKS'
+  user.confirmed_at = Time.now
+  user.city_id = City.last.id
+  user.save(validate: false)
+end
+
+# User.create(email: 'user@p2p.example', nickname: Faker::TvShows::RickAndMorty.character, first_name: Faker::Name.first_name,
+#             second_name: Faker::Name.last_name, role: 'user', encrypted_password: '$2a$11$w3tai7ejKU89Em1cH8NFLeZ2NgZd39jzM4JLFWCIqazatYYlv6vKS',
+#             confirmed_at: Time.now, city_id: City.last.id)
